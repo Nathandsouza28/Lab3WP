648556850f05d173db2eac3a7b51ebdc45972d5b
Nathandsouza28 <90825199+Nathandsouza28@users.noreply.github.com>
2021-11-23 10:17:56 +0400

    COmpleye\

--- apis/routes.js
+++ apis/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const clientController = require('../controllers/clientController');
 
 //define a router and create routes
 const router = express.Router();
@@ -7,8 +8,16 @@ const router = express.Router();
 //routes for dynamic processing of products
 //-----------------------------------------------
 //route for listing all products
-router.get('/api/catalogue', productController.getCatalogue);
+router.get('/api/catalog', productController.getCatalog);
 router.get('/api/article/:id', productController.getProductByID);
 
 //export router
+module.exports = router;
+
+//routes for dynamic processing of clients
+//-----------------------------------------------
+//route for registration
+router.post("/api/register", clientController.registerControl);
+//route for login
+router.post("/api/login", clientController.loginControl);
 module.exports = router;
--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require("express");
 //creating app
 const app = express();
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-});
+
+//handling static HTML and EJS templates
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
+
+//route for contacts
 app.get("/contacts", (req, res) => {
   res.render("contacts");
+});
+
+//route for register
+app.get("/api/register", (req, res) => {
+ res.render("register"); 
+});
+
+//route for login
+app.get("/api/login", (req, res) => {
+ res.render("login"); 
+});
+
+//pass requests to the router middleware
+const router = require("./apis/routes");
+app.use(router);
+
+
+//route for catalog
+app.get("/api/catalog", (req, res) => {
+  res.render("catalog");
+});
+
+//route for clients
+app.get("/api/clients", (req, res) => {
+  res.render("clients");
 });
